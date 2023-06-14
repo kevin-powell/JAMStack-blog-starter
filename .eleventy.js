f0d11461ea8a69975f5285df756db71435ddbd59
Arona <87335321+tarinbuys@users.noreply.github.com>
2023-06-14 10:14:35 +0000

    modified:   .eleventy.js 	modified:   package-lock.json     modified:   package.json 	new file:   src/_includes/article-snippet.njk 	new file:   src/_includes/article.njk 	new file:   src/_includes/base.njk 	new file:   src/_includes/footer.njk 	new file:   src/_includes/header.njk 	deleted:    src/all-articles.html 	new file:   src/blog.njk 	new file:   src/blog/blog.json 	deleted:    src/index.html 	new file:   src/index.njk 	deleted:    src/individual-article.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('.src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+      });
+
     return {
         dir: {
             input: "src",
